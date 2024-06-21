b595081a8206168039c18f9556fd27bfe87050ac
cotete <feliperibeirots@gmail.com>
2024-06-21 17:10:37 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner/Banner';
+
 
 function App() {
   return (
     <div className="App">
+      <Banner/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Eu amo react!!
         </p>
         <a
           className="App-link"
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner(){
+    return(
+        //JSX
+        <header className="banner">
+            <img src="images/banner.png" alt="O banner da pagina principal"/>
+        </header>
+      
+    )
+}
+
+export default Banner
